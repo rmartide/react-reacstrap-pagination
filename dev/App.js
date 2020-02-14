fcdd2b6bab98b32c5180ac637ae94cb47def8c19
Rodrigo Martín de Lamo <mcgod69esp@gmail.com>
2020-02-14 22:06:47 +0100

    Another refactor to add memoization and to make sure when the props change the component updates as expected

--- dev/App.js
+++ dev/App.js
@@ -3,27 +3,35 @@ import PaginationComponent from '../src/PaginationComponent';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = {
+    defaultActivePage: 1,
+    size: 'lg',
+    maxPaginationNumbers: 10
   }
 
   handleSelected = option => {
+    this.setState({defaultActivePage: option})
     console.log('selected', option);
   };
 
   render() {
     return (
-      <div>
-        <h1>Boilerplate</h1>
-        <PaginationComponent
-          size="lg"
-          totalItems={30}
-          pageSize={1}
-          onSelect={this.handleSelected}
-          maxPaginationNumbers={10}
-          defaultActivePage={11}
-        />
-      </div>
+      <>
+        <div>
+          <h1>Boilerplate</h1>
+          <PaginationComponent
+            size={this.state.size}
+            totalItems={31}
+            pageSize={1}
+            onSelect={this.handleSelected}
+            maxPaginationNumbers={this.state.maxPaginationNumbers}
+            defaultActivePage={this.state.defaultActivePage}
+          />
+        </div>
+        <div>
+          <button onClick={()=>{this.setState({defaultActivePage:11, maxPaginationNumbers: 5})}}>Change default active page</button>
+        </div>
+      </>
     );
   }
 }
--- src/PaginationComponent.js
+++ src/PaginationComponent.js
@@ -1,56 +1,83 @@
-import React, { Component } from "react";
+import React, { PureComponent } from "react";
 import { Pagination, PaginationItem, PaginationLink } from "reactstrap";
 import PropTypes from "prop-types";
+import memoize from 'memoize-one';
 
-class PaginationComponent extends Component {
+class PaginationComponent extends PureComponent {
   constructor(props) {
     super(props);
-
-    this.pages = this.getNumberOfPages(this.props);
 
     this.state = {
       activePage: this.props.defaultActivePage
     };
   }
 
-  getNumberOfPages = props => {
-    const auxPages = props.totalItems / props.pageSize;
-    let pages = parseInt(auxPages, 10);
-    pages += pages !== auxPages ? 1 : 0;
-    return pages;
-  };
+  // Since we want to not have to worry about when this changes and since it won't change much
+  // Memoize will remember the last parameter and only execute when they change
+  getNumberOfPages = memoize(
+    props => {
+      const auxPages = props.totalItems / props.pageSize;
+      let pages = parseInt(auxPages, 10);
+      pages += pages !== auxPages ? 1 : 0;
+      return pages;
+    }
+  );
 
   paginationItems = () => {
+
+    if (this.props.defaultActivePage !== this.defaultActivePage) {
+      this.defaultActivePage = this.props.defaultActivePage;
+      this.activePage = this.defaultActivePage;
+    }
+
+    const pages = this.getNumberOfPages(this.props);
     let items = [];
+    const { activePage } = this;
+    const { firstPageText, previousPageText, nextPageText, lastPageText } = this.props;
+
     // Since first and last PaginationNumber depend on activepage there's no reason to have them on the state
     // So we just make the calculations when we need them
-    const firstPaginationNumber = this.getFirstPaginationNumber(this.state.activePage);
-    const lastPaginationNumber = this.getLastPaginationNumber(firstPaginationNumber);
-    items.push(this.firstOrLastPagItem(this.props.firstPageText, 1));
-    items.push(this.nextOrPreviousPagItem(this.props.previousPageText, 1, "l"));
-    for (
-      let i = firstPaginationNumber;
-      i <= lastPaginationNumber;
-      i++
-    ) {
-      items.push(this.numberedPagItem(i));
+    const firstPaginationNumber = this.getFirstPaginationNumber(activePage, pages);
+    const lastPaginationNumber = this.getLastPaginationNumber(firstPaginationNumber, pages);
+
+    // Elements first and previous
+    items.push(this.firstOrLastPagItem(firstPageText, 1));
+    items.push(this.nextOrPreviousPagItem(previousPageText, 1, "l"));
+
+    // Page numbers
+    for (let i = firstPaginationNumber; i <= lastPaginationNumber; i++) {
+      items.push(this.numberedPagItem(i, activePage));
     }
-    items.push(
-      this.nextOrPreviousPagItem(this.props.nextPageText, this.pages, "r")
-    );
-    items.push(this.firstOrLastPagItem(this.props.lastPageText, this.pages));
+    // Elements next and last
+    items.push(this.nextOrPreviousPagItem(nextPageText, pages, "r"));
+    items.push(this.firstOrLastPagItem(lastPageText, pages));
     return items;
   };
 
-  getLastPaginationNumber = (firstPaginationNumber) => {
+  getFirstPaginationNumber = (activePage, pages) => {
+    const distance = Math.floor(this.props.maxPaginationNumbers / 2);
+    const newFPNumber = activePage - distance;
+    const newLPNumber = activePage + distance;
+    let result = 1;
+    if (newFPNumber <= distance) {
+      result = 1;
+    } else if (newLPNumber <= pages) {
+      result = newFPNumber;
+    } else if (newLPNumber >= pages) {
+      result = pages - this.props.maxPaginationNumbers + 1;
+    }
+    return result;
+  };
+
+  getLastPaginationNumber = (firstPaginationNumber, pages) => {
     const minNumberPages = Math.min(
-      this.pages,
+      pages,
       this.props.maxPaginationNumbers
     );
     return firstPaginationNumber + minNumberPages - 1;
   };
 
-  numberedPagItem = i => {
+  numberedPagItem = (i, activePage) => {
     let minWidth = "43.5px";
     if (this.props.size === "lg") {
       minWidth = "71px"
@@ -61,7 +88,7 @@ class PaginationComponent extends Component {
       <PaginationItem
         key={i}
         id={`pagebutton${i}`}
-        active={this.state.activePage === i}
+        active={activePage === i}
         onClick={this.handleClick}
       >
         <PaginationLink style={{ minWidth }}>{i}</PaginationLink>
@@ -73,7 +100,7 @@ class PaginationComponent extends Component {
     return (
       <PaginationItem
         key={name}
-        disabled={this.state.activePage === page}
+        disabled={this.activePage === page}
         onClick={e => this.handleSelectNextOrPrevious(direction)}
       >
         <PaginationLink>{name}</PaginationLink>
@@ -90,7 +117,7 @@ class PaginationComponent extends Component {
     return (
       <PaginationItem
         key={name}
-        disabled={this.state.activePage === page}
+        disabled={this.activePage === page}
         onClick={() => this.handleClick(event)}
       >
         <PaginationLink>{name}</PaginationLink>
@@ -110,9 +137,10 @@ class PaginationComponent extends Component {
   };
 
   handleSelectNextOrPrevious = direction => {
-    const activePage = this.state.activePage;
+    const { activePage, props } = this;
+    const pages = this.getNumberOfPages(props);
     if (
-      (direction === "r" && activePage === this.pages) ||
+      (direction === "r" && activePage === pages) ||
       (direction === "l" && activePage === 1)
     )
       return;
@@ -123,27 +151,12 @@ class PaginationComponent extends Component {
   };
 
   changePaginationState = (newActivePage) => {
+    this.activePage = newActivePage;
     this.setState({
       activePage: newActivePage
     });
-
     this.props.onSelect(newActivePage);
   }
-
-  getFirstPaginationNumber = (activePage) => {
-    const distance = Math.floor(this.props.maxPaginationNumbers / 2);
-    const newFPNumber = activePage - distance;
-    const newLPNumber = activePage + distance;
-    let result = 1;
-    if (newFPNumber <= distance) {
-        result = 1;
-    } else if (newLPNumber <= this.pages) {
-      result = newFPNumber;
-    } else if (newLPNumber >= this.pages) {
-      result = this.pages - this.props.maxPaginationNumbers + 1;
-    }
-    return result;
-  };
 
   render() {
     return <Pagination size={this.props.size}>{this.paginationItems()}</Pagination>;
@@ -165,7 +178,7 @@ PaginationComponent.propTypes = {
 
 PaginationComponent.defaultProps = {
   maxPaginationNumbers: 5,
-  activePage: 1,
+  defaultActivePage: 1,
   firstPageText: "First",
   previousPageText: "Previous",
   nextPageText: "Next",
